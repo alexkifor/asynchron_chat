dd3aaf07fa8580d006f8280e4e88de20aa922b0c
Aleksandr <8712060@gmail.com>
2023-06-03 23:02:19 +0300

    lession_5

--- main/client.py
+++ main/client.py
@@ -1,10 +1,14 @@
 from socket import *
-from main.variables import *
+from variables import *
 import sys
 import json
 import time
+sys.path.append('../')
 from main.utils import send_msg, get_msg
+import logging
+import log.config_client
 
+CLI_LOG = logging.getLogger('client')
 
 def create_msg(account_name="Guest"):
     '''
@@ -21,6 +25,7 @@ def create_msg(account_name="Guest"):
         } 
     }
 
+    CLI_LOG.debug(f"generated message {msg} to send the server from {account_name}")
     return msg
 
 
@@ -30,9 +35,12 @@ def handler_server_msg(data):
     :param data:
     :return:
     '''
-    if 'response' in data:
+    CLI_LOG.debug(f'parsing response from the server {data}')
+    if 'response' in data:    
         if data['response'] == 200:
+            CLI_LOG.info(f'successful, client received a response from sever  : 200: OK')
             return '200: OK'
+        CLI_LOG.info(f'successful, client received a response from sever: 400: {data["error"]} ')
         return f'400: {data["error"]}'
     raise ValueError
 
@@ -43,27 +51,31 @@ def main():
     Функция загружает параметры из командной строки адрес и порт. Создает соединение с сервером
     Отправляет сообщение на сервер и принимает ответ от сервера.
     '''
+    CLI_LOG.info(f'trying to start socket on the client')
     try:
         server_port = int(sys.argv[2])
-        server_ip = sys.argv[1]
+        server_ip = sys.argv[4]
         if server_port < 1024 or server_port > 65535:
             raise ValueError
     except IndexError:
         server_ip = DEFAULT_IP
         server_port = DEFAULT_PORT
     except ValueError:
-        print('Значение порта должно быть целым числом от 1024 до 65535')
+        CLI_LOG.critical(f"starting client on an invalid {server_port} port")
         sys.exit(1)
 
     s = socket(AF_INET, SOCK_STREAM) # Создать сокет TCP
+    CLI_LOG.info(f'started client socket on {server_port} port with ip_address {server_ip}')
     s.connect((server_ip, server_port)) # Соединиться с сервером
+    CLI_LOG.info(f'server connection established')
     presence_msg = create_msg()
     send_msg(s, presence_msg)
+    CLI_LOG.info(f'sending message {presence_msg} from client to server')
     try:
         answer = handler_server_msg(get_msg(s))
         print(answer)
     except (ValueError, json.JSONDecodeError):  
-        print('Принято некорретное сообщение от клиента.')
+        CLI_LOG.error(f'invalid message received from client')
         
 
 if __name__ == '__main__':
