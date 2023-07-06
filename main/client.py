57d821d4ffc96158509c9630d054053bf7a85971
Aleksandr <8712060@gmail.com>
2023-07-06 09:17:43 +0300

    lession_12

--- main/client.py
+++ main/client.py
@@ -12,158 +12,349 @@ import log.config_client
 from decos import log
 from discripts import Port, Addr
 from metaclasses import ClientVerifier
+from clcient_db import ClientPool
+
+
+
 
 CLI_LOG = logging.getLogger('client')
+sock_lock = threading.Lock()
+database_lock = threading.Lock()
+class ClientSender(threading.Thread, metaclass=ClientVerifier):
+    def __init__(self, account_name, sock, database):
+        self.account_name = account_name
+        self.sock = sock
+        self.database = database
+        super().__init__()
+
+    def create_exit_message(self):
+        return {
+            'action': 'exit',    
+            'time': time.time(),
+            'account_name': self.account_name
+        }
+
+    def create_message(self):
+        to = input('Введите получателя сообщения: ')
+        message = input('Введите сообщение для отправки: ')
+
+        with database_lock:
+            if not self.database.check_user(to):
+                CLI_LOG.error(f'Попытка отправить сообщение незарегистрированому получателю: {to}')
+                return
+
+        message_dict = {
+            'action': 'message',
+            'from': self.account_name,
+            'to': to,
+            'time': time.time(),
+            'msg_text': message
+        }
+        CLI_LOG.debug(f'Сформирован словарь сообщения: {message_dict}')
+
+        with database_lock:
+            self.database.save_message(self.account_name , to , message)
+
+        with sock_lock:
+            try:
+                send_msg(self.sock, message_dict)
+                CLI_LOG.info(f'Отправлено сообщение для пользователя {to}')
+            except OSError as err:
+                if err.errno:
+                    CLI_LOG.critical('Потеряно соединение с сервером.')
+                    exit(1)
+                else:
+                    CLI_LOG.error('Не удалось передать сообщение. Таймаут соединения')
+
+    def run(self):
+        self.print_help()
+        while True:
+            command = input('Введите команду: ')
+            if command == 'message':
+                self.create_message()
+            elif command == 'help':
+                self.print_help()
+            elif command == 'exit':
+                with sock_lock:
+                    try:
+                        send_msg(self.sock, self.create_exit_message())
+                    except:
+                        pass
+                    print('Завершение соединения.')
+                    CLI_LOG.info('Завершение работы по команде пользователя.')
+                time.sleep(0.5)
+                break
+
+            elif command == 'contacts':
+                with database_lock:
+                    contacts_list = self.database.get_contacts()
+                for contact in contacts_list:
+                    print(contact)
+
+            elif command == 'edit':
+                self.edit_contacts()
+
+            elif command == 'history':
+                self.print_history()
+
+            else:
+                print('Команда не распознана, попробойте снова. help - вывести поддерживаемые команды.')
+
+    def print_help(self):
+        print('Поддерживаемые команды:')
+        print('message - отправить сообщение. Кому и текст будет запрошены отдельно.')
+        print('history - история сообщений')
+        print('contacts - список контактов')
+        print('edit - редактирование списка контактов')
+        print('help - вывести подсказки по командам')
+        print('exit - выход из программы')
+
+    def print_history(self):
+        ask = input('Показать входящие сообщения - in, исходящие - out, все - просто Enter: ')
+        with database_lock:
+            if ask == 'in':
+                history_list = self.database.get_history(to_who=self.account_name)
+                for message in history_list:
+                    print(f'\nСообщение от пользователя: {message[0]} от {message[3]}:\n{message[2]}')
+            elif ask == 'out':
+                history_list = self.database.get_history(from_who=self.account_name)
+                for message in history_list:
+                    print(f'\nСообщение пользователю: {message[1]} от {message[3]}:\n{message[2]}')
+            else:
+                history_list = self.database.get_history()
+                for message in history_list:
+                    print(f'\nСообщение от пользователя: {message[0]}, пользователю {message[1]} от {message[3]}\n{message[2]}')
+
+    
+    def edit_contacts(self):
+        ans = input('Для удаления введите del, для добавления add: ')
+        if ans == 'del':
+            edit = input('Введите имя удаляемного контакта: ')
+            with database_lock:
+                if self.database.check_contact(edit):
+                    self.database.del_contact(edit)
+                else:
+                    CLI_LOG.error('Попытка удаления несуществующего контакта.')
+        elif ans == 'add':
+            edit = input('Введите имя создаваемого контакта: ')
+            if self.database.check_user(edit):
+                with database_lock:
+                    self.database.add_contact(edit)
+                with sock_lock:
+                    try:
+                        add_contact(self.sock , self.account_name, edit)
+                    except ConnectionError:
+                        CLI_LOG.error('Не удалось отправить информацию на сервер.')
+
+
+class ClientReader(threading.Thread, metaclass=ClientVerifier):
+    def __init__(self, account_name, sock, database):
+        self.account_name = account_name
+        self.sock = sock
+        self.database = database
+        super().__init__()
+
+    def run(self):
+        while True:
+            time.sleep(1)
+            with sock_lock:
+                try:
+                    message = get_msg(self.sock)
+
+                except UnicodeDecodeError:
+                    CLI_LOG.error(f'Не удалось декодировать полученное сообщение.')
+                except OSError as err:
+                    if err.errno:
+                        CLI_LOG.critical(f'Потеряно соединение с сервером.')
+                        break
+        
+                except (ConnectionError, ConnectionAbortedError, ConnectionResetError, json.JSONDecodeError):
+                    CLI_LOG.critical(f'Потеряно соединение с сервером.')
+                    break
+                else:
+                    if 'action' in message and message['action'] == 'message' and 'from' in message and 'to' in message \
+                            and 'msg_text' in message and message['to'] == self.account_name:
+                        print(f"\nПолучено сообщение от пользователя {message['from']}:\n{message['msg_text']}")
+                        with database_lock:
+                            try:
+                                self.database.save_message(message['from'], self.account_name, message['msg_text'])
+                            except:
+                                CLI_LOG.error('Ошибка взаимодействия с базой данных')
+
+                        CLI_LOG.info(f"Получено сообщение от пользователя {message['from']}:\n{message['msg_text']}")
+                    else:
+                        CLI_LOG.error(f'Получено некорректное сообщение с сервера: {message}')
+
+
+@log
+def create_presence(account_name):
+    out = {
+        'action': 'presence',
+        'time': time.time(),
+        'user': {
+            'account_name': account_name
+        }
+    }
+    CLI_LOG.debug(f'Сформировано "presence" сообщение для пользователя {account_name}')
+    return out
+
+
+@log
+def process_response_ans(message):
+    CLI_LOG.debug(f'Разбор приветственного сообщения от сервера: {message}')
+    if 'response' in message:
+        if message['response'] == 200:
+            return '200 : OK'
+        elif message['response'] == 400:
+            raise ConnectionError(f"400 : {message['error']}")
+    raise Exception('response')
+
 
 @log
 def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', default = DEFAULT_IP, nargs='?') 
-    parser.add_argument('-p', default= DEFAULT_PORT, nargs='?')
-    parser.add_argument('-n', default=None, nargs='?')
-    name_space = parser.parse_args(sys.argv[1:])
-    serv_addr = name_space.a
-    serv_port = name_space.p
-    client_name = name_space.n
+    parser.add_argument('addr', default=DEFAULT_IP, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-n', '--name', default=None, nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+    client_name = namespace.name
+    if not 1023 < server_port < 65536:
+        CLI_LOG.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}. Допустимы адреса с 1024 до 65535. Клиент завершается.')
+        exit(1)
+
+    return server_address, server_port, client_name
+
+def contacts_list_request(sock, name):
+    CLI_LOG.debug(f'Запрос контакт листа для пользователся {name}')
+    req = {
+        'action': 'get_contacts',
+        'time': time.time(),
+        'user': name
+    }
+    CLI_LOG.debug(f'Сформирован запрос {req}')
+    send_msg(sock, req)
+    ans = get_msg(sock)
+    CLI_LOG.debug(f'Получен ответ {ans}')
+    if 'response' in ans and ans['response'] == 202:
+        return ans['data_list']
+    else:
+        raise ConnectionError
+
+
+def add_contact(sock, username, contact):
+    CLI_LOG.debug(f'Создание контакта {contact}')
+    req = {
+        'action': 'add',
+        'time': time.time(),
+        'user': username,
+        'account_name': contact
+    }
+    send_msg(sock, req)
+    ans = get_msg(sock)
+    if 'response' in ans and ans['response'] == 200:
+        pass
+    else:
+        raise Exception('Ошибка создания контакта')
+    print('Удачное создание контакта.')
+
+
+def user_list_request(sock, username):
+    CLI_LOG.debug(f'Запрос списка известных пользователей {username}')
+    req = {
+        'action': 'get_users',
+        'time': time.time(),
+        'account)name': username
+    }
+    send_msg(sock, req)
+    ans = get_msg(sock)
+    if 'response' in ans and ans['response'] == 202:
+        return ans['data_list']
+    else:
+        raise ConnectionError
+
+
+def remove_contact(sock, username, contact):
+    CLI_LOG.debug(f'Создание контакта {contact}')
+    req = {
+        'action': 'remove',
+        'time': time.time(),
+        'user': username,
+        'account_name': contact
+    }
+    send_msg(sock, req)
+    ans = get_msg(sock)
+    if 'response' in ans and ans['response'] == 200:
+        pass
+    else:
+        raise Exception('Ошибка удаления клиента')
+    print('Удачное удаление')
+
+
+def database_load(sock, database, username):
+    try:
+        users_list = user_list_request(sock, username)
+    except ConnectionError:
+        CLI_LOG.error('Ошибка запроса списка известных пользователей.')
+    else:
+        database.add_users(users_list)
+
+    try:
+        contacts_list = contacts_list_request(sock, username)
+    except Exception:
+        CLI_LOG.error('Ошибка запроса списка контактов.')
+    else:
+        for contact in contacts_list:
+            database.add_contact(contact)
+
+
+def main():
+    print('Консольный месседжер. Клиентский модуль')
+    server_address, server_port, client_name = arg_parser()
     if not client_name:
-        client_name = input('Enter the username: ')
-    return serv_addr, serv_port, client_name
+        client_name = input('Введите имя пользователя: ')
+    else:
+        print(f'Клиентский модуль запущен с именем: {client_name}')
 
+    CLI_LOG.info(
+        f'Запущен клиент с парамертами: адрес сервера: {server_address} , порт: {server_port}, имя пользователя: {client_name}')
 
-class Client(metaclass=ClientVerifier):
-    port = Port()
-    addr = Addr()
-    
-    def __init__(self, serv_addr, serv_port, client_name):
-        self.port = serv_port
-        self.addr = serv_addr
-        self.client_name = client_name
-        self.sock = None
+    try:
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.settimeout(1)
 
-    def init_sock(self):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Создать сокет TCP
-            s.connect((self.addr, self.port)) # Соединиться с сервером
-            self.sock = s
-            CLI_LOG.info(f'server connection established')
-            send_msg(s, self.create_presence())
-            serv_answer = self.proc_response_serv_ans(get_msg(self.sock))
-            CLI_LOG.info(f'sending message {serv_answer} from server to client')
-            print('server connection established')
-        except (ValueError, json.JSONDecodeError):  
-            CLI_LOG.error(f'invalid message received from client')
-            sys.exit(1)
-        except (ConnectionAbortedError, ConnectionError, ConnectionResetError):
-            CLI_LOG.error(f'the connection to the server {self.addr} was lost')
-            sys.exit(1)
+        transport.connect((server_address, server_port))
+        send_msg(transport, create_presence(client_name))
+        answer = process_response_ans(get_msg(transport))
+        CLI_LOG.info(f'Установлено соединение с сервером. Ответ сервера: {answer}')
+        print(f'Установлено соединение с сервером.')
+    except json.JSONDecodeError:
+        CLI_LOG.error('Не удалось декодировать полученную Json строку.')
+        exit(1)
+    except (ConnectionRefusedError, ConnectionError):
+        CLI_LOG.critical(
+            f'Не удалось подключиться к серверу {server_address}:{server_port}, конечный компьютер отверг запрос на подключение.')
+        exit(1)
+    else:
 
+        database = ClientPool(client_name)
+        database_load(transport, database, client_name)
+        module_sender = ClientSender(client_name, transport, database)
+        module_sender.daemon = True
+        module_sender.start()
+        CLI_LOG.debug('Запущены процессы')
+        module_receiver = ClientReader(client_name, transport, database)
+        module_receiver.daemon = True
+        module_receiver.start()
 
-    def create_presence(self):
-        out = {
-            'action' : 'presence',
-            'time' : time.time(),
-            'user' : {
-                'account_name' : self.client_name
-            }   
-        }
-        CLI_LOG.debug(f"generated message 'presence' for user {self.client_name}")
-        return out
-    
-   
-    def proc_response_serv_ans(self, data):
-        CLI_LOG.debug(f"generated hello_message from the server: {data}")
-        if 'response' in data:
-            if data['response'] == 200:
-                return '200 : OK'
-            elif data['response'] == 400:
-                raise ValueError(f"400 : {data['error']}")
-        ValueError(f'missing response')
-
-   
-    def create_exit_msg(self):
-        out = {'action' : 'exit',
-            'time' : time.time(),
-            'account_name' : self.client_name
-            }
-        return out
-
-      
-    def create_msg(self):
-        recipient = input('enter the distination of the message: ')
-        msg = input('enter message to send: ')
-
-        msg_dict = {
-            "action": "message",
-            "from" : self.client_name,
-            "to" : recipient,
-            "time": time.time(),
-            "msg_text" : msg
-            } 
-        CLI_LOG.debug(f"generated dict_message {msg_dict}")
-        try:
-            send_msg(self.sock, msg_dict)
-            CLI_LOG.info(f"on server user {msg_dict['from']} to send {msg_dict['msg_text']} for {msg_dict['to']}")
-        except:
-            CLI_LOG.critical('Connection with server was lost')
-            sys.exit(1)
-
-
-    def msg_from_server(self):
-        while True:
-            try:
-                msg = get_msg(self.sock)
-                if 'action' in msg and msg['action'] == 'message' and 'from' in msg and 'to' in msg \
-                and 'msg_text' in msg and msg['to'] == self.client_name:    
-                    print(f"\nTo get message from {msg['from']}: {msg['msg_text']}")
-                    CLI_LOG.info(f"message received from user {msg['from']}: {msg['msg_text']}")
-                else:
-                    CLI_LOG.error(f"bad message received from server: {msg}")
-                    sys.exit(1)
-            except (ValueError, OSError, ConnectionError, ConnectionAbortedError, json.JSONDecodeError):
-                CLI_LOG.critical(f"error sending response from server")
-                break
-
-  
-    def user_interactive(self):
-        print(f'You - {self.client_name}')
-        print('Command help:')
-        print('message - to send message. Enter the recipient and text of the message in the appropriate fields')
-        print('exit - exit from the program')
-        while True:
-            cmd = input('Enter the command: message or exit: ')
-            if cmd == 'message':
-                self.create_msg()
-            elif cmd == 'exit':
-                send_msg(self.sock, self.create_exit_msg())
-                print('Connection ended.')
-                CLI_LOG.info(f"Connection ended after user {self.client_name} command")
-                time.sleep(1)
-                break
-            else:
-                print('bad command, try again')
-
-    @property
-    def main_loop(self):
-        print('Console chat, client modul.')
-        self.init_sock()
-        receiver = threading.Thread(target=self.msg_from_server, daemon=True)
-        receiver.start()
-        user_interface = threading.Thread(target=self.user_interactive, daemon=True)
-        user_interface.start()
-        CLI_LOG.debug(f'The client started processes for sending and receiving messages')
         while True:
             time.sleep(1)
-            if receiver.is_alive() and user_interface.is_alive():
+            if module_receiver.is_alive() and module_sender.is_alive():
                 continue
             break
 
 
-def main():
-    serv_addr, serv_port, client_name = arg_parser()       
-    client = Client(serv_addr, serv_port, client_name)
-    client.main_loop    
-
-        
-
 if __name__ == '__main__':
-     main()
+    main()
--- main/server_db.py
+++ main/server_db.py
@@ -27,8 +27,24 @@ class ServerPool:
             self.ip_addr = ip_addr
             self.port = port
 
-    def __init__(self):
-        self.db_engine = create_engine(SERVER_DB, echo=False, pool_recycle=7200)
+    class UsersContacts:
+        def __init__(self, user, contact):
+            self.id = None
+            self.user = user
+            self.contact = contact
+
+
+    class UsersHistory:
+        def __init__(self, user):
+            self.id = None
+            self.user = user
+            self.sent = 0
+            self.accepted = 0
+
+
+    def __init__(self, path):
+        print(path)
+        self.db_engine = create_engine(f'sqlite:///{path}', echo=False, pool_recycle=7200, connect_args={'check_same_thread': False})
         self.metadata = MetaData()
         users_table = Table('Users', self.metadata,
             Column('id', Integer, primary_key=True),
@@ -49,17 +65,29 @@ class ServerPool:
             Column('ip_addr', String),
             Column('port', String)
         )
+        contacts = Table('Contacts', self.metadata,
+            Column('id', Integer, primary_key=True),
+            Column('user', ForeignKey('Users.id')),
+            Column('contact', ForeignKey('Users.id'))
+        )
+        users_hist_tbl = Table('History', self.metadata,
+            Column('id', Integer, primary_key=True),
+            Column('user', ForeignKey('Users.id')),
+            Column('sent', Integer),
+            Column('accepted', Integer)
+        )
         self.metadata.create_all(self.db_engine)
         mapper(self.AllUsers, users_table)
         mapper(self.ActiveUsers, active_users_table)
         mapper(self.LoginHistory, user_login_history)
+        mapper(self.UsersContacts, contacts)
+        mapper(self.UsersHistory, users_hist_tbl)
         Session = sessionmaker(bind=self.db_engine)
         self.session = Session()
         self.session.query(self.ActiveUsers).delete()
         self.session.commit()
 
     def user_login(self, username, ip_addr, port):
-        print(username, ip_addr, port)
         result = self.session.query(self.AllUsers).filter_by(name=username)
         if result.count():
             user = result.first()
@@ -68,6 +96,8 @@ class ServerPool:
             user = self.AllUsers(username)
             self.session.add(user)
             self.session.commit()
+            user_in_hist = self.UsersHistory(user.id)
+            self.session.add(user_in_hist)
         new_activ_user = self.ActiveUsers(user.id, ip_addr, port, datetime.datetime.now())
         self.session.add(new_activ_user)
         history = self.LoginHistory(user.id, datetime.datetime.now(), ip_addr, port)
@@ -78,6 +108,33 @@ class ServerPool:
         user = self.session.query(self.AllUsers).filter_by(name=user_name).first()
         self.session.query(self.ActiveUsers).filter_by(user=user.id).delete()
         self.session.commit()
+
+    def process_message(self, sender, recipient):
+        sender = self.session.query(self.AllUsers).filter_by(name=sender).first().id
+        recipient = self.session.query(self.AllUsers).filter_by(name=recipient).first().id
+        sender_row = self.session.query(self.UsersHistory).filter_by(user=sender).first()
+        sender_row.sent += 1
+        recipient_row = self.session.query(self.UsersHistory).filter_by(user=recipient).first()
+        recipient_row.accepted += 1
+
+    def add_contact(self, user, contact):
+        user = self.session.query(self.AllUsers).filter_by(name=user).first()
+        contact = self.session.query(self.AllUsers).filter_by(name=contact).first()
+        if not contact or self.session.query(self.UsersContacts).filter_by(user=user.id, contact=contact.id).count():
+            return
+        contact_row = self.UsersContacts(user.id, contact.id)
+        self.session.add(contact_row)
+        self.session.commit()
+
+    def remove_contact(self, user, contact):
+        user = self.session.query(self.AllUsers).filter_by(name=user).first()
+        contact = self.session.query(self.AllUsers).filter_by(name=contact).first()
+        if not contact:
+            return
+        print(self.session.query(self.UsersContacts).filter(self.UsersContacts.user == user.id,
+                                                            self.UsersContacts.contact == contact.id).delete())
+        self.session.commit()
+
 
     def users_list(self):
         query = self.session.query(
@@ -105,15 +162,40 @@ class ServerPool:
         if user_name:
             query = query.filter(self.AllUsers.name == user_name)
             return query.all()
+        
+    def get_contact(self, user_name):
+        user = self.session.query(self.AllUsers).filter_by(name=user_name).one()
+        query = self.session.query(self.UsersContacts, self.AllUsers.name).filter_by(user=user.id).join(self.AllUsers, \
+        self.UsersContacts.contact == self.AllUsers.id)
+        return [contact[1] for contact in query.all()]
+
+    def msg_history(self):
+        query = self.session.query(
+            self.AllUsers.name,
+            self.AllUsers.last_login,
+            self.UsersHistory.sent,
+            self.UsersHistory.accepted
+        ).join(self.AllUsers) 
+        return query.all()   
+        
 
 
 if __name__ == '__main__':
     test_db = ServerPool()
     test_db.user_login('client_1', '192.168.1.1', 7777)
     test_db.user_login('client_2', '192.168.1.2', 8001)
+    test_db.user_login('client_3', '192.168.1.2', 8002)
+    test_db.user_login('client_4', '192.168.1.2', 8003)
+    test_db.user_login('client_5', '192.168.1.2', 8004)
+    print(test_db.users_list())
     print(test_db.active_users_list())
     test_db.user_logout('client_1')
     print(test_db.active_users_list())
     test_db.login_history('client_1')
-    print(test_db.users_list())
+    test_db.add_contact('client_2', 'client_1')
+    test_db.add_contact('client_3', 'client_2')
+    test_db.add_contact('client_5', 'client_3')
+    test_db.remove_contact('client_2', 'client_1')
+    test_db.process_message('client_1', 'client_4')
+    print(test_db.msg_history())
 
